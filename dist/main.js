a2d1173844ce0f079eedd98a8553b829e73f674d
Evelyn Gomez <74394654+evelyn-gomez@users.noreply.github.com>
2024-02-27 15:43:36 -0800

    simple fetch data

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"hello mundo\");\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+eval("console.log(\"hello mundo\");\nconst main = document.querySelector(\".main\");\nconst searchContainer = document.querySelector(\".search-container\"); \nconst searchInput = document.querySelector(\".search-container input\");\nconst searchButton = document.querySelector(\".search-container button\"); \n\nconst apiKey = \"9b80e0ff9b8349449c4181957231212\";\nconst apiURL =  \"http://api.weatherapi.com/v1/\";\n\nsearchButton.addEventListener(\"click\", ()=>{\n  const location = searchInput.value; \n  \n  if(location){\n    fetchWeather(location);\n  }else{\n    console.log(\"empty -- need city name or zipcode\"); \n  }\n})\n\nfunction fetchWeather(location){\n  const url = `${apiURL}current.json?key=${apiKey}&q=${location}&aqi=yes`; \n\n  fetch(url)\n  .then(resp => resp.json())\n  .then(data => {\n    if (data.error) {\n      throw data.error\n    }\n    console.log(data);\n  })\n  .catch(error => console.log(error))\n}\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
